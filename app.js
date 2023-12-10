e59e720736472cf2230b4d3a5085a61df4d1ec34
raje1966 <rajendra_kulkarni1@rediffmail.com>
2023-12-10 17:03:14 +0530

    Adding file 'digital-clock' to github

---
+++ app.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    function buildClock() {
+    
+    const date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes: minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.querySelector('#clock-hours').innerText = hours;
+    document.querySelector('#clock-minutes').innerText = minutes;
+    document.querySelector('#clock-seconds').innerText = seconds;
+
+    setTimeout(buildClock, 1000)
+
+    }
+    buildClock();
+
+}
